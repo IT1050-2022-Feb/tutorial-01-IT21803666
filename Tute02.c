f1ec828315c71267897ba0f5c81fd2ebaf46993b
Semin Balasuriya <110513760+IT21803666@users.noreply.github.com>
2022-08-03 16:57:31 +0000

    developed a program to calculate amount to be paid for a rented vehicle

--- Tute02.c
+++ Tute02.c
@@ -1,24 +1,26 @@
-/*Exercise 2 - Selection
-
-Write a program to calculate the amount to be paid for a rented vehicle.
-
-•	Input the distance the van has travelled
-•	The first 30 km is at a rate of 50/= per km.
-•	The remaining distance is calculated at the rate of 40/= per km.
-
-
-e.g.
-
-Distance -> 20
-Amount = 20 x 50 = 1000
-
-
-Distance -> 50
-Amount = 30 x 50 + (50-30) x 40 = 2300*/
+//program to calculate the amount to be paid for a rented vehicle
 
 #include <stdio.h>
+int main (void) //function main begins program execution 
+{
+  float distance, amount, remainingDist; //declaring variables for distance, amount and remaining distance
 
-int main() {
+  printf("Input distance:"); //prompt
+  scanf("%f", &distance); //user inputs distance 
+
+  if ( distance <= 30 ) //using selection 
+  {
+    amount = distance * 50.00; //calculation for amount
+    printf("Amount to be paid is %.2f", amount); //prompt
+  }
+    else if ( distance > 30 )
+    {
+      remainingDist = ( distance - 30.00 ) * 40.00;
+      amount = remainingDist + ( 30.00 * 50.00 ); //calculation for amount 
+
+      printf("Amount to be paid is Rs.%.2f", amount); //prompt
+    }
+
+  return 0; //end of main function
   
-  return 0;
 }
