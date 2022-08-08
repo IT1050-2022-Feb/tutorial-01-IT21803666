e66a20686718ff1167248fa2361b411ac58cd470
Semin Balasuriya <110513760+IT21803666@users.noreply.github.com>
2022-08-08 05:53:39 +0000

    developed a programme with 3 functions

--- Tute04.c
+++ Tute04.c
@@ -1,19 +1,48 @@
-/*Exercise 4 - Functions
+//programe developed with 3 functions 
 
-Implement the three functions minimum(), maximum() and multiply() below the main() function.
+#include <stdio.h> 
 
-Do not change the code given in the main() function when you are implementing your solution.*/
-
-#include <stdio.h>
-
-int main() {
+int min ( int x, int y ); 
+int max ( int x, int y );
+int mult ( int x, int y );
+int main() //function main begins programme execution 
+{
    int no1, no2;
+   int maximum, minimum, multiply;
+  
    printf("Enter a value for no 1 : ");
    scanf("%d", &no1);
    printf("Enter a value for no 2 : ");
    scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
+
+   minimum = min ( no1, no2 );
+   printf("%d\n", minimum );
+
+   maximum = max ( no1, no2 );
+   printf("%d\n", maximum);
+
+   multiply = mult ( no1, no2 );
+   printf("%d\n", multiply);
+  
    return 0;
+}
+int min ( int x, int y )
+{
+  if ( x < y)
+    return x;
+  else 
+    return y;
+}
+
+int max ( int x, int y)
+{
+  if ( x > y )
+    return x;
+  else 
+  return y;
+}
+
+int mult ( int x, int y )
+{
+  return x * y;
 }
